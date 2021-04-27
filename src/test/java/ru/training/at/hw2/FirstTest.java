5c365ef23079f3e9b762055619102b6eeea7e2b0
Konstantin Nadonenko <konstantin.nadonenko@ukg.com>
2021-04-27 22:02:39 +0300

    lesson 2

---
+++ src/test/java/ru/training/at/hw2/FirstTest.java
@@ -0,0 +1,44 @@
+package ru.training.at.hw2;
+
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class FirstTest {
+
+    private WebDriverWait webDriverWait;
+    WebDriver webDriver;
+
+    private WebElement webElement;
+
+    @Test
+    public void simpleTest() {
+        System.setProperty("webdriver.chrome.driver", "");
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts()
+                .implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.navigate().to("https://www.epam.com");
+        webElement = webDriver.findElement(By.xpath("//span[text() = 'Home']"));
+        webElement.click();
+        webDriverWait = new WebDriverWait(webDriver, 10);
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
+        assertEquals("EPAM | Enterprise Software Development, Design & Consulting",
+                webDriver.getTitle());
+    }
+
+    @After
+    public void clear() {
+        webDriver.close();
+    }
+
+}
